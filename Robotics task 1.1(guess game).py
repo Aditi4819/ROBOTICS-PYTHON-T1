f7bd84909d1e24fb1f270f67b5e453e2c3502fe8
Aditi4819 <99380927+Aditi4819@users.noreply.github.com>
2022-06-22 09:59:34 +0530

    Add files via upload

--- Robotics task 1.1(guess game).py
+++ Robotics task 1.1(guess game).py
@@ -1,24 +1,27 @@
 import random
-
 randomNumber = random.randrange(1000,9999)
-print("I’m thinking of a number in the range 1000-9999. You have ten tries to guess it.")
-guess = False
-c = 0
-
-while guess is False and c < 10:
-    userChoice = int(input("Guess ?"))
+print("I am Thinking of a number between 1000 and 9999 and you have 10 tries to guess it")
+guess= False
+c=0
+while guess is False and c<10:
+    userChoice = int(input("Guess?"))
     if userChoice < 1000 :
         print("Wrong choice!!! Choose a 4-digit number")
         continue
     elif userChoice>9999 :
         print("Wrong choice!!! Choose a 4-digit number")
         continue
-    c += 1
+    c+=1
     if userChoice == randomNumber:
         guess = True
         print("R")
+    elif userChoice == randomNumber:
+        guess = True
+        choice_str = str(userChoice)
+        for i , val in enumerate(str(randomNumber)):
+            if choice_str[i] != val:
+                print("A")
     elif userChoice != randomNumber:
         print("W")
     if c == 10:
         print("You are running out of tries!!! Try again...")
-        print("End of program")
